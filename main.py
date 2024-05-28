a7d5195df14d3633691b25950714c8be68c3fb94
Moshfeq Shahrani <moshahrani@yahoo.com>
2024-05-28 11:47:29 -0700

    added storage class with attributes for each package

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+class Custom_Hash_Table:
+
+
+class Storage: 
+    def __init__(self, ID, address, city, state, zip_code, delivery_time, date, status):
+        self.ID = ID
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.delivery_time = delivery_time
+        self.date = date
+        self.status = status
+
+
+
